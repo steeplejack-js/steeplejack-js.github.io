015b3240bdde15729783573245187d8737ad1643
Simon Emms <simon@simonemms.com>
2017-05-20 22:32:15 +0100

    Stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,31 +8,7 @@
 
 /* Files */
 
+
 (function () {
-  $(function () {
-    attachApp();
-  });
-
-  function setSidebarWrapper () {
-    var wrapperWidth = $('#sidebar').parents('.sidebar-wrapper').width();
-
-    console.log(wrapperWidth);
-
-    $('#sidebar.affix').width(wrapperWidth);
-  }
-
-  function attachApp () {
-
-    /* Trigger HLJS */
-    hljs.initHighlightingOnLoad();
-
-    // $(window).resize(setSidebarWrapper);
-    // setSidebarWrapper();
-
-    var affix = $('#sidebar').affix('checkPosition');
-
-    affix.on('affixed.bs.affix');
-
-  }
 
 })();
