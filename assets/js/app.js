ecdb252c0181eddbdeb86c4c6dfa2128d4dccca7
Simon Emms <simon@simonemms.com>
2017-05-23 20:55:08 +0100

    Styled up the docs page

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,15 @@
 
 (function () {
 
+  /* Initialize HighlightJS */
   hljs.initHighlightingOnLoad();
+
+  /* Get the child nav */
+  $('#site-content h3').each(function (key, value) {
+    var id = $(value).attr('id');
+    var name = $(value).text();
+
+    $('#sidebar-child ul').append('<li><a href="#' + id + '">' + name + '</a></li>');
+  });
 
 })();
